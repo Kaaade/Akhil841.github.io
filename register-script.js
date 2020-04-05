97594957a9295866d23dfab12bbd2b62003ecad9
Akhil841 <44041958+Akhil841@users.noreply.github.com>
2020-04-05 10:09:17 -0700

    Update register-script.js

--- register-script.js
+++ register-script.js
@@ -1,3 +1,53 @@
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "test", // update me
+      password: "testP@ssw0rd" // update me
+    },
+    type: "default"
+  },
+  server: "akanserver.database.windows.net", // update me
+  options: {
+    database: "Authenticators", //update me
+    encrypt: true
+  }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    queryDatabase();
+  }
+});
+
+function queryDatabase(usrname, hashpw) {
+  // Read all rows from table
+  const request = new Request(
+    `INSERT INTO Authenticators (Username, Password) VALUES (` + usrname + `, ` + hashpw + `);`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+    }
+  );
+
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+  });
+
+  connection.execSql(request);
+}
 function makeAccount() {
   var output = document.getElementById("result");
   var password1 = document.getElementById("pw");
